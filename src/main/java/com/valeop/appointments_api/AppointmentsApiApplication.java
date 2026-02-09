1dd784df55c778eef5a0b13b4cb0e83c148abfc4
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-09 12:27:25 -0500

    Initial configurations done

---
+++ src/main/java/com/valeop/appointments_api/AppointmentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.valeop.appointments_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valeop/appointments_api/AppointmentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valeop.appointments_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
