e9e0dafaf51395e7b1132979d301fdd1f4150ef6
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-09 12:30:24 -0500

    Role entity created

---
+++ src/main/java/com/valeop/appointments_api/model/Role.java
@@ -0,0 +1,47 @@
+package com.valeop.appointments_api.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer roleId;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String roleName;
+
+    public Role() {
+    }
+
+    @Autowired
+    public Role(Integer roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
