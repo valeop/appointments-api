c3f3b9e84a56dd3f7da503c7de1c03f839e116ec
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-09 12:41:58 -0500

    PersonController created

---
+++ src/main/java/com/valeop/appointments_api/controller/PersonController.java
@@ -0,0 +1,77 @@
+package com.valeop.appointments_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.valeop.appointments_api.dto.PersonDTO;
+import com.valeop.appointments_api.service.impl.PersonServiceImpl;
+
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+    private final PersonServiceImpl personServiceImpl;
+
+    @Autowired
+    public PersonController(PersonServiceImpl personServiceImpl) {
+        this.personServiceImpl = personServiceImpl;
+    }
+
+    @GetMapping("")
+    public String isPersonWorking() {
+        return "Hello, Person working.";
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<PersonDTO>> getList() {
+        List<PersonDTO> personsList = personServiceImpl.getPersonList();
+        return ResponseEntity.status(HttpStatus.OK).body(personsList);
+    }
+
+    @GetMapping(value = "/list", params = "id")
+    public ResponseEntity<PersonDTO> getPersonById(@RequestParam(value = "id") Integer personId) {
+        PersonDTO personExisting = personServiceImpl.getPersonById(personId);
+        return ResponseEntity.status(HttpStatus.OK).body(personExisting);
+    }
+
+    @GetMapping(value = "/list", params = "id-card")
+    public ResponseEntity<PersonDTO> getPersonByIdentityCard(@RequestParam(value = "id-card") String identityCard) {
+        PersonDTO personExisting = personServiceImpl.getPersonByIdentityCard(identityCard);
+        return new ResponseEntity<>(personExisting, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<PersonDTO> createPerson(@Valid @RequestBody PersonDTO person) {
+        PersonDTO newPerson = personServiceImpl.createPerson(person, person.getGender().getGenderId(),
+                person.getBloodType().getBloodTypeId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(newPerson);
+    }
+
+    @PutMapping("/update/{personId}")
+    public ResponseEntity<PersonDTO> updatePerson(@Valid @RequestBody PersonDTO person,
+            @PathVariable Integer personId) {
+        PersonDTO personBBDD = personServiceImpl.updatePerson(person, personId);
+        return ResponseEntity.ok(personBBDD);
+    }
+
+    @DeleteMapping("/delete/{personId}")
+    public ResponseEntity<PersonDTO> deletePerson(@PathVariable Integer personId) {
+        PersonDTO personDeleted = personServiceImpl.deletePerson(personId);
+        return ResponseEntity.ok(personDeleted);
+    }
+}
