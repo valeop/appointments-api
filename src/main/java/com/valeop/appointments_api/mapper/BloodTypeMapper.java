96e8b2ae9a0bf0d38da269c58778d9eb7e9dedc0
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-11 17:52:02 -0500

    BloodTypeMapper methods changed to contribute with BloodType DTO's created

--- src/main/java/com/valeop/appointments_api/mapper/BloodTypeMapper.java
+++ src/main/java/com/valeop/appointments_api/mapper/BloodTypeMapper.java
@@ -2,7 +2,9 @@ package com.valeop.appointments_api.mapper;
 
 import org.springframework.stereotype.Component;
 
-import com.valeop.appointments_api.dto.BloodTypeDTO;
+import com.valeop.appointments_api.dto.bloodtype.BloodTypeResponseDTO;
+import com.valeop.appointments_api.dto.bloodtype.CreateBloodTypeDTO;
+import com.valeop.appointments_api.dto.bloodtype.UpdateBloodTypeDTO;
 import com.valeop.appointments_api.model.BloodType;
 
 @Component
@@ -11,11 +13,19 @@ public class BloodTypeMapper {
     private BloodTypeMapper() {
     }
 
-    public static BloodTypeDTO toDTO(BloodType bloodType) {
-        return new BloodTypeDTO(bloodType.getBloodTypeId(), bloodType.getBloodTypeName());
+    public static BloodType fromCreateBloodTypeDTO(CreateBloodTypeDTO dto) {
+        BloodType bloodType = new BloodType();
+        bloodType.setBloodTypeName(dto.bloodTypeName());
+        return bloodType;
     }
 
-    public static BloodType toEntity(BloodTypeDTO bloodTypeDTO) {
-        return new BloodType(bloodTypeDTO.getBloodTypeId(), bloodTypeDTO.getBloodTypeName());
+    public static void updateFromDTO(UpdateBloodTypeDTO dto, BloodType bloodType) {
+        if (!dto.bloodTypeName().isBlank()) {
+            bloodType.setBloodTypeName(dto.bloodTypeName());
+        }
+    }
+
+    public static BloodTypeResponseDTO toResponseDTO(BloodType bloodType) {
+        return new BloodTypeResponseDTO(bloodType.getBloodTypeId(), bloodType.getBloodTypeName());
     }
 }
