9b463e93fc229a89ab6c9bd4d6925182e5a9e27e
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-11 16:07:09 -0500

    GenderMapper modified to include Gender DTO's as parameters and return objects

--- src/main/java/com/valeop/appointments_api/mapper/GenderMapper.java
+++ src/main/java/com/valeop/appointments_api/mapper/GenderMapper.java
@@ -2,7 +2,9 @@ package com.valeop.appointments_api.mapper;
 
 import org.springframework.stereotype.Component;
 
-import com.valeop.appointments_api.dto.GenderDTO;
+import com.valeop.appointments_api.dto.gender.CreateGenderDTO;
+import com.valeop.appointments_api.dto.gender.GenderResponseDTO;
+import com.valeop.appointments_api.dto.gender.UpdateGenderDTO;
 import com.valeop.appointments_api.model.Gender;
 
 @Component
@@ -11,11 +13,19 @@ public class GenderMapper {
     private GenderMapper() {
     }
 
-    public static GenderDTO toDTO(Gender gender) {
-        return new GenderDTO(gender.getGenderId(), gender.getGenderName());
+    public static Gender fromCreateGenderDTO(CreateGenderDTO dto) {
+        Gender gender = new Gender();
+        gender.setGenderName(dto.genderName());
+        return gender;
     }
 
-    public static Gender toGender(GenderDTO genderDTO) {
-        return new Gender(genderDTO.getGenderId(), genderDTO.getGenderName());
+    public static void updateFromDTO(UpdateGenderDTO dto, Gender gender) {
+        if (!dto.genderName().isBlank()) {
+            gender.setGenderName(dto.genderName());
+        }
+    }
+
+    public static GenderResponseDTO toResponseDTO(Gender gender) {
+        return new GenderResponseDTO(gender.getGenderId(), gender.getGenderName());
     }
 }
