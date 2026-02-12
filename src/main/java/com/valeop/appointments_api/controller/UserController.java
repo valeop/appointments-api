66010fa0ea5d4afb6b252b1ee13b2828343d3a7b
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-11 23:51:37 -0500

    UserController created

---
+++ src/main/java/com/valeop/appointments_api/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.valeop.appointments_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.valeop.appointments_api.dto.user.CreateUserDTO;
+import com.valeop.appointments_api.dto.user.UserResponseDTO;
+import com.valeop.appointments_api.service.impl.UserServiceImpl;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserServiceImpl userServiceImpl;
+
+    @Autowired
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
+    }
+
+    @GetMapping("")
+    public String isUserWorking() {
+        return "Hello, User is working.";
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponseDTO>> getUsersList() {
+        List<UserResponseDTO> userList = userServiceImpl.getUsersList();
+        return ResponseEntity.ok(userList);
+    }
+
+    @GetMapping("/all/{userId}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Integer userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userServiceImpl.getUserById(userId));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody CreateUserDTO userDTO) {
+        UserResponseDTO userSaved = userServiceImpl.createUser(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userSaved);
+    }
+}
