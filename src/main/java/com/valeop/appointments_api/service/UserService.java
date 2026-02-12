60340379679e7d43558d37fd74130ecb928dcbbd
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-11 23:50:51 -0500

    UserService created

---
+++ src/main/java/com/valeop/appointments_api/service/UserService.java
@@ -0,0 +1,22 @@
+package com.valeop.appointments_api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.valeop.appointments_api.dto.user.CreateUserDTO;
+import com.valeop.appointments_api.dto.user.UpdateUserDTO;
+import com.valeop.appointments_api.dto.user.UserResponseDTO;
+
+@Service
+public interface UserService {
+    List<UserResponseDTO> getUsersList();
+
+    UserResponseDTO getUserById(Integer userId);
+
+    UserResponseDTO createUser(CreateUserDTO userDTO);
+
+    UserResponseDTO updateUser(UpdateUserDTO userDTO, Integer userId);
+
+    UserResponseDTO deleteUser(Integer userId);
+}
