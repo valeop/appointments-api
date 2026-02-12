df41a7ffd7eafc108e55da39739b4d956f39c046
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-11 23:47:31 -0500

    UserResponseDTO created

---
+++ src/main/java/com/valeop/appointments_api/dto/user/UserResponseDTO.java
@@ -0,0 +1,12 @@
+package com.valeop.appointments_api.dto.user;
+
+import com.valeop.appointments_api.model.Person;
+import com.valeop.appointments_api.model.Role;
+
+public record UserResponseDTO(
+        Integer userId,
+        Person person,
+        Role role,
+        String email,
+        String passwordHash) {
+}
