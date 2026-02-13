dba814841ef682df1d76dc8db4427a6812c85233
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-13 18:10:56 -0500

    Removing imported library

--- src/main/java/com/valeop/appointments_api/controller/ServiceTypeController.java
+++ src/main/java/com/valeop/appointments_api/controller/ServiceTypeController.java
@@ -2,7 +2,6 @@ package com.valeop.appointments_api.controller;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
