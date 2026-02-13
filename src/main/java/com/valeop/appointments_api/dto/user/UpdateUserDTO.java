952fae2bccca7f1ca2acc8cc13248c5a34035da1
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-13 14:42:24 -0500

    passwordHash attribute removed in order to implement security practices

--- src/main/java/com/valeop/appointments_api/dto/user/UpdateUserDTO.java
+++ src/main/java/com/valeop/appointments_api/dto/user/UpdateUserDTO.java
@@ -4,9 +4,8 @@ import com.valeop.appointments_api.model.Person;
 import com.valeop.appointments_api.model.Role;
 
 public record UpdateUserDTO(
-        Integer userId,
-        Person person,
-        Role role,
-        String email,
-        String passwordHash) {
+                Integer userId,
+                Person person,
+                Role role,
+                String email) {
 }
