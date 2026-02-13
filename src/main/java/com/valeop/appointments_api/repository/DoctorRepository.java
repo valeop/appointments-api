0b62994a60fbe41f28e694bfc1f937b590fc27ac
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-13 11:18:39 -0500

    DoctorRepository created

---
+++ src/main/java/com/valeop/appointments_api/repository/DoctorRepository.java
@@ -0,0 +1,16 @@
+package com.valeop.appointments_api.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.valeop.appointments_api.model.Doctor;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+    List<Doctor> findAll();
+
+    Optional<Doctor> findByDoctorId(Integer doctorId);
+}
