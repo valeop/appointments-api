1743bc56d25eae659d13be483bac22c16a1ed6f6
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-18 15:35:28 -0500

    DoctorServiceMapper created

---
+++ src/main/java/com/valeop/appointments_api/mapper/DoctorServiceMapper.java
@@ -0,0 +1,27 @@
+package com.valeop.appointments_api.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.valeop.appointments_api.dto.doctorservice.DoctorServiceResponseDTO;
+import com.valeop.appointments_api.model.Doctor;
+import com.valeop.appointments_api.model.DoctorService;
+import com.valeop.appointments_api.model.Service;
+
+@Component
+public class DoctorServiceMapper {
+
+    private DoctorServiceMapper() {
+    }
+
+    public static DoctorService fromCreateDoctorService(Doctor doctor, Service service) {
+        DoctorService doctorService = new DoctorService();
+        doctorService.setDoctor(doctor);
+        doctorService.setService(service);
+        return doctorService;
+    }
+
+    public static DoctorServiceResponseDTO toResponseDTO(DoctorService doctorService) {
+        return new DoctorServiceResponseDTO(doctorService.getDoctorServiceId(), doctorService.getDoctor(),
+                doctorService.getService());
+    }
+}
