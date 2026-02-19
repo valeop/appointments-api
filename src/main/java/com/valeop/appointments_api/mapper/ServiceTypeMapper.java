cac956c614eced9acd6a51723ba16e008dffe0ee
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-19 15:00:37 -0500

    Update method removed from ServiceTypeMapper

--- src/main/java/com/valeop/appointments_api/mapper/ServiceTypeMapper.java
+++ src/main/java/com/valeop/appointments_api/mapper/ServiceTypeMapper.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 import com.valeop.appointments_api.dto.servicetype.CreateServiceTypeDTO;
 import com.valeop.appointments_api.dto.servicetype.ServiceTypeResponseDTO;
-import com.valeop.appointments_api.dto.servicetype.UpdateServiceTypeDTO;
 import com.valeop.appointments_api.model.ServiceType;
 
 @Component
@@ -16,12 +15,6 @@ public class ServiceTypeMapper {
         ServiceType serviceType = new ServiceType();
         serviceType.setServiceTypeName(dto.serviceTypeName());
         return serviceType;
-    }
-
-    public static void updateFromDTO(UpdateServiceTypeDTO dto, ServiceType serviceType) {
-        if (!dto.serviceTypeName().isBlank()) {
-            serviceType.setServiceTypeName(dto.serviceTypeName());
-        }
     }
 
     public static ServiceTypeResponseDTO toResponseDTO(ServiceType serviceType) {
