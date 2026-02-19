fafba0a189752845a785ba2f0f9889a28b0e5c83
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-19 11:56:24 -0500

    UpdateAppointmentDTO created

---
+++ src/main/java/com/valeop/appointments_api/dto/appointment/UpdateAppointmentDTO.java
@@ -0,0 +1,13 @@
+package com.valeop.appointments_api.dto.appointment;
+
+import java.time.LocalDateTime;
+
+import com.valeop.appointments_api.model.DoctorService;
+import com.valeop.appointments_api.model.User;
+
+public record UpdateAppointmentDTO(
+        Integer appointmentId,
+        LocalDateTime appointmentDateTime,
+        User user,
+        DoctorService doctorService) {
+}
