ea3adf6ace1f4abf0a1e730e536301e577836837
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-19 14:59:50 -0500

    Update method removed from BloodTypeMapper

--- src/main/java/com/valeop/appointments_api/mapper/BloodTypeMapper.java
+++ src/main/java/com/valeop/appointments_api/mapper/BloodTypeMapper.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 import com.valeop.appointments_api.dto.bloodtype.BloodTypeResponseDTO;
 import com.valeop.appointments_api.dto.bloodtype.CreateBloodTypeDTO;
-import com.valeop.appointments_api.dto.bloodtype.UpdateBloodTypeDTO;
 import com.valeop.appointments_api.model.BloodType;
 
 @Component
@@ -17,12 +16,6 @@ public class BloodTypeMapper {
         BloodType bloodType = new BloodType();
         bloodType.setBloodTypeName(dto.bloodTypeName());
         return bloodType;
-    }
-
-    public static void updateFromDTO(UpdateBloodTypeDTO dto, BloodType bloodType) {
-        if (!dto.bloodTypeName().isBlank()) {
-            bloodType.setBloodTypeName(dto.bloodTypeName());
-        }
     }
 
     public static BloodTypeResponseDTO toResponseDTO(BloodType bloodType) {
