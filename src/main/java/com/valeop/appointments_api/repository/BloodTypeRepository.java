819132c98151a28d4fe52540f2f8d68f1107773e
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-19 15:07:16 -0500

    @Repository annotation added in BloodTypeRepository

--- src/main/java/com/valeop/appointments_api/repository/BloodTypeRepository.java
+++ src/main/java/com/valeop/appointments_api/repository/BloodTypeRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.valeop.appointments_api.model.BloodType;
 
+@Repository
 public interface BloodTypeRepository extends JpaRepository<BloodType, Integer> {
 
     List<BloodType> findAll();
