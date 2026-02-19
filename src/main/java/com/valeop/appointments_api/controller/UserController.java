4a392a4e82b512785970c20f73584c8e44a0a533
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-19 16:16:53 -0500

    Methods to modified in UserController

--- src/main/java/com/valeop/appointments_api/controller/UserController.java
+++ src/main/java/com/valeop/appointments_api/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.valeop.appointments_api.service.impl.UserServiceImpl;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -46,26 +48,27 @@ public class UserController {
 
     @GetMapping("/all/{userId}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Integer userId) {
-        return ResponseEntity.status(HttpStatus.OK).body(userServiceImpl.getUserById(userId));
+        UserResponseDTO userFound = userServiceImpl.getUserById(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(userFound);
     }
 
     @PostMapping("/create")
-    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody CreateUserDTO userDTO) {
-        UserResponseDTO userSaved = userServiceImpl.createUser(userDTO);
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody CreateUserDTO createDTO) {
+        UserResponseDTO userSaved = userServiceImpl.createUser(createDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(userSaved);
     }
 
     @PutMapping(value = "/update", params = "id")
-    public ResponseEntity<UserResponseDTO> updateUser(@Valid @RequestBody UpdateUserDTO updateUserDTO,
+    public ResponseEntity<UserResponseDTO> updateUser(@Valid @RequestBody UpdateUserDTO updateDTO,
             @RequestParam(value = "id") Integer userId) {
-        UserResponseDTO userUpdated = userServiceImpl.updateUser(updateUserDTO, userId);
+        UserResponseDTO userUpdated = userServiceImpl.updateUser(updateDTO, userId);
         return ResponseEntity.status(HttpStatus.OK).body(userUpdated);
     }
 
     @PutMapping(value = "/update/password", params = "id")
-    public ResponseEntity<UserResponseDTO> updateUserPassword(@Valid @RequestBody UpdatePasswordDTO updatePasswordDTO,
+    public ResponseEntity<UserResponseDTO> updateUserPassword(@Valid @RequestBody UpdatePasswordDTO updateDTO,
             @RequestParam(value = "id") Integer userId) {
-        UserResponseDTO userUpdated = userServiceImpl.updateUserPassword(updatePasswordDTO, userId);
+        UserResponseDTO userUpdated = userServiceImpl.updateUserPassword(updateDTO, userId);
         return ResponseEntity.ok(userUpdated);
     }
 
