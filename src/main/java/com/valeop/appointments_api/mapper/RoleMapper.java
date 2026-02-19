700d3912b07c6de82f02828088bf3f80e9a43a5c
Valeria OP <valeria.ortiz2001@gmail.com>
2026-02-19 15:00:22 -0500

    Update method removed from RoleMapper

--- src/main/java/com/valeop/appointments_api/mapper/RoleMapper.java
+++ src/main/java/com/valeop/appointments_api/mapper/RoleMapper.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 import com.valeop.appointments_api.dto.role.CreateRoleDTO;
 import com.valeop.appointments_api.dto.role.RoleResponseDTO;
-import com.valeop.appointments_api.dto.role.UpdateRoleDTO;
 import com.valeop.appointments_api.model.Role;
 
 @Component
@@ -17,12 +16,6 @@ public class RoleMapper {
         Role role = new Role();
         role.setRoleName(dto.roleName());
         return role;
-    }
-
-    public static void updateFromDTO(UpdateRoleDTO dto, Role role) {
-        if (!dto.roleName().isBlank()) {
-            role.setRoleName(dto.roleName());
-        }
     }
 
     public static RoleResponseDTO toResponseDTO(Role role) {
