108f62fc328c8d715de9ecf6d2f15ac414aa8ca1
Valeria OP <valeria.ortiz2001@gmail.com>
2026-03-10 12:31:37 -0500

    AuthService created

---
+++ src/main/java/com/valeop/appointments_api/auth/AuthService.java
@@ -0,0 +1,66 @@
+package com.valeop.appointments_api.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.valeop.appointments_api.dto.user.UpdatePasswordDTO;
+import com.valeop.appointments_api.dto.user.UserResponseDTO;
+import com.valeop.appointments_api.exceptions.BadRequestException;
+import com.valeop.appointments_api.exceptions.ResourceNotFoundException;
+import com.valeop.appointments_api.jwt.JwtService;
+import com.valeop.appointments_api.mapper.UserMapper;
+import com.valeop.appointments_api.model.User;
+import com.valeop.appointments_api.repository.UserRepository;
+
+import io.jsonwebtoken.Claims;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, JwtService jwtService,
+            AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public AuthResponse login(AuthRequest authRequest) {
+        authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(authRequest.email(), authRequest.password()));
+
+        User user = userRepository.findByEmail(authRequest.email())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with email: " + authRequest.email()));
+
+        String token = jwtService.generateAccessToken(user);
+
+        return new AuthResponse(token, "Bearer", jwtService.extractClaim(token, Claims::getExpiration),
+                user.getUsername(),
+                user.getRole().getRoleName());
+    }
+
+    public UserResponseDTO updatePassword(UpdatePasswordDTO updateDTO, Integer userId) {
+        User userFound = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "User does not exist with ID #" + userId));
+        if (!passwordEncoder.matches(updateDTO.currentPassword(), userFound.getPassword())) {
+            throw new BadRequestException("Invalid current password");
+        }
+        if (!updateDTO.newPassword().equals(updateDTO.confirmNewPassword())) {
+            throw new BadRequestException("New password does not match with password confirmed");
+        }
+        if (passwordEncoder.matches(updateDTO.newPassword(), userFound.getPassword())) {
+            throw new BadRequestException("New password shouldn't be the same as current one");
+        }
+
+        userFound.setPasswordHash(passwordEncoder.encode(updateDTO.newPassword()));
+        User userUpdated = userRepository.save(userFound);
+        return UserMapper.toResponseDTO(userUpdated);
+    }
+}
