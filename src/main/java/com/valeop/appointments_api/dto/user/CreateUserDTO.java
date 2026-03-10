1a804ac8b8f83d3623c3f18c88417172021ec99d
Valeria OP <valeria.ortiz2001@gmail.com>
2026-03-10 12:36:09 -0500

    Attribute "max" changed to "min" in CreateUserDTO

--- src/main/java/com/valeop/appointments_api/dto/user/CreateUserDTO.java
+++ src/main/java/com/valeop/appointments_api/dto/user/CreateUserDTO.java
@@ -9,8 +9,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateUserDTO(
-        @NotNull(message = "Person should not be empty.") Person person,
-        @NotNull(message = "Role should not be empty.") Role role,
-        @Email(message = "Email should be well structured.") @NotBlank(message = "Email should not be empty.") @Size(max = 40, message = "Email characters limit exceeded. (40)") String email,
-        @NotBlank(message = "Password should not be empty.") @Size(max = 255, message = "Password characters limit exceeded. (255)") String passwordHash) {
+                @NotNull(message = "Person should not be empty.") Person person,
+                @NotNull(message = "Role should not be empty.") Role role,
+                @Email(message = "Email should be well structured.") @NotBlank(message = "Email should not be empty.") @Size(max = 40, message = "Email characters limit exceeded. (40)") String email,
+                @NotBlank(message = "Password should not be empty.") @Size(min = 255, message = "Minimun password characters is 8.") String passwordHash) {
 }
