d60b84b17a642854a10479ed8c267e086d678faf
Valeria OP <valeria.ortiz2001@gmail.com>
2026-04-15 12:07:38 -0500

    UserSecurityDetails class created

---
+++ src/main/java/com/valeop/appointments_api/model/UserSecurityDetails.java
@@ -0,0 +1,58 @@
+package com.valeop.appointments_api.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserSecurityDetails implements UserDetails {
+
+    private final String email;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserSecurityDetails(User user) {
+        this.email = user.getEmail();
+        this.password = user.getPasswordHash();
+        this.authorities = List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().getRoleName()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+}
