35c9683a75eeb42f11a625050d3ef666215c95cd
Valeria OP <valeria.ortiz2001@gmail.com>
2026-04-15 12:06:46 -0500

    findAllByUser() and findAllByDoctorService() methods added

--- src/main/java/com/valeop/appointments_api/repository/AppointmentRepository.java
+++ src/main/java/com/valeop/appointments_api/repository/AppointmentRepository.java
@@ -7,10 +7,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.valeop.appointments_api.model.Appointment;
+import com.valeop.appointments_api.model.DoctorService;
+import com.valeop.appointments_api.model.User;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
     List<Appointment> findAll();
 
     Optional<Appointment> findByAppointmentId(Integer appointmentId);
+
+    List<Appointment> findAllByUser(User user);
+
+    List<Appointment> findAllByDoctorService(DoctorService doctorService);
 }
